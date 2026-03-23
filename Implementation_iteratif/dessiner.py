3b9f8a0677b2a8edacd319df3672054374842cba
Nyel <nayel.siyoucef@gmail.com>
2026-03-23 11:58:09 +0100

    je suis en train de changer dessiner pour utilise matplotlib

--- Implementation_iteratif/dessiner.py
+++ Implementation_iteratif/dessiner.py
@@ -1,31 +1,62 @@
 import var_poo as var
 import fun_iter as fun
-import pygame as pg 
-pg.init()
-surf = pg.display.set_mode((var.haut,var.haut))
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
 
 continuer = True
 save = -1
+t=0
 
-while continuer:
+get_pos = np.vectorize(lambda planete: planete.pos)
+
+
+
+traj = []
+for _ in range(var.t_max):
+    if not continuer:
+        break
+    t+=var.dt
+    #si on fait juste traj.append(var.sys), cela renvoie un pointeur tjr vers la même valeur
+    traj.append(get_pos(var.planetes))
+    #fun.actual met a jour les coordonées, vitesses, accélérations, de chacune des planètes dans le tab var.sys auquel l'on ajoute des dimensions
     save, continuer = fun.actual(var.planetes,var.t,var.res,var.i_sat)
-    var.t+=var.dt
-    #si la touche q est pressée, on quitte
-    for event in pg.event.get():
-        if event.type == pg.QUIT :
-            continuer = False
-    touches = pg.key.get_pressed()
-    if touches[pg.K_q]:
-        continuer = False
-    surf.fill((0,0,0))
-    #on affiche le temps
-    text = pg.font.Font(None, 20).render(f"Temps: {var.t}", True, (255, 255, 255))
-    surf.blit(text, (10, 10))
-    
 
-    for i in range(var.planetes.shape[0]):
-            pg.draw.circle(surf,var.planetes[i].couleur,(var.planetes[i].pos[0],var.planetes[i].pos[1]),var.planetes[i].r)
+traj_np = np.array(traj)
 
-    pg.display.update()
-print(save)
-pg.quit()
+print(traj_np.shape)
+fig, axis = plt.subplots()
+
+axis.set_xlim([0,var.long])
+axis.set_ylim([0,var.haut])
+
+axis.set_title("Simulation N-corps")
+axis.set_aspect("equal")
+
+points = []
+for i in range(len(var.planetes)):
+    p, = axis.plot([], [], 'o',
+                   markersize=var.planetes[i].r,
+                   color=var.planetes[i]/255)
+    points.append(p)
+
+def update_data(frame):
+    #dans anim.set_data, on doit avoir un tab de shape (2,N,len(t))
+    for i, p in enumerate(points):
+        p.set_data(
+            [traj_np[0, i, frame]],
+            [traj_np[1, i, frame]]
+        )
+    return points,
+
+
+animation = FuncAnimation(
+    fig=fig,
+    func= update_data,
+    frames= len(traj),
+    interval = 25,
+    )
+
+#animation.save("nom.gif")
+plt.show()
